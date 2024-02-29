5a0a68fef11e5f9774cddfab3b31cb5e8f9e9645
Headsh0t <88953209+Ziedny28@users.noreply.github.com>
2024-02-29 11:13:29 +0700

    Update ChangeLevel.cs

--- Assets/LearnScene/Scripts/ChangeLevel.cs
+++ Assets/LearnScene/Scripts/ChangeLevel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//please explain the code with comment
 public class ChangeLevel : MonoBehaviour
 {
    public void ChangeScene(string levelName)
